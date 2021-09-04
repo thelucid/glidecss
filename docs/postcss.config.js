71ba64608550a1aa78a973018e63322379074009
Jamie Hill <jamie@thelucid.com>
2021-09-04 17:07:00 +0100

    Introduce the concept of tones

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -2,10 +2,10 @@ const purgecss = require('@fullhuman/postcss-purgecss')
 
 module.exports = {
   plugins: [
-    purgecss({
-      content: ['./index.html'],
-      defaultExtractor: (content) => content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
-      safelist: ['menu__link--h2', 'menu__link--h3', 'is-current']
-    })
+    // purgecss({
+    //   content: ['./index.html'],
+    //   defaultExtractor: (content) => content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
+    //   safelist: ['menu__link--h2', 'menu__link--h3', 'is-current']
+    // })
   ]
 }
